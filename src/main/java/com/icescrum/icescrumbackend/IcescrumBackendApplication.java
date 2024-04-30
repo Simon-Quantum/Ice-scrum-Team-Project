e0a390884640baca41b2ecaddded7f4ada644f1c
Luca Massei <luca.massei@student.unimelb.edu.au>
2024-05-01 02:20:53 +1000

    Initial commit for Spring Boot backend setup.

---
+++ src/main/java/com/icescrum/icescrumbackend/IcescrumBackendApplication.java
@@ -0,0 +1,13 @@
+package com.icescrum.icescrumbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcescrumBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcescrumBackendApplication.class, args);
+	}
+
+}
