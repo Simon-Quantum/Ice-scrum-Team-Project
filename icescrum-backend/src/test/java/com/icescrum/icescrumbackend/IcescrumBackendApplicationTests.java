c9978eeafb78152bb8b7f5e4d2a4a5a4ef21551b
Luca Massei <luca.massei@student.unimelb.edu.au>
2024-05-01 02:52:43 +1000

    • Moved backend files to separate folder.
    • Added .gitignore file.

---
+++ icescrum-backend/src/test/java/com/icescrum/icescrumbackend/IcescrumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icescrum.icescrumbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcescrumBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
